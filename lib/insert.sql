24c96036097808649451232e3a8a24419d492434
Raveen K Sharma <raveen08@gmail.com>
2020-09-02 21:07:10 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,4 +5,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergea
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3, 'male', 'brown', 'angry', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3, 'male', 'brown', 'angry', 1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 3, 'male', 'brown', 'angry', 1);
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('NULL', 3, 'male', 'brown', 'angry', 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, 'male', 'brown', 'angry', 1);
